78322b18ef3643882b2bac2bf8b3ebb21ceafcce
Duc Dao <43461796+ddao5@users.noreply.github.com>
2021-04-18 01:01:42 -0400

    Refactored services. closes #11

--- pet-clinic-data/src/main/java/springframework/sfgpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/springframework/sfgpetclinic/services/PetService.java
@@ -3,12 +3,7 @@ package springframework.sfgpetclinic.services;
 
 import springframework.sfgpetclinic.model.Pet;
 
-import java.util.Set;
 
-public interface PetService {
-    Pet findById(Long id);
+public interface PetService extends CrudService<Pet, Long> {
 
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
 }
