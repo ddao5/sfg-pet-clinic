039b4c7c3b5fe2ca385f42e5a49beab15553afed
Duc Dao <43461796+ddao5@users.noreply.github.com>
2021-04-26 01:42:43 -0400

    Added Owners List and Updated Controller. Closes #16

--- pet-clinic-web/src/main/java/springframework/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/springframework/sfgpetclinic/controllers/VetController.java
@@ -2,9 +2,11 @@ package springframework.sfgpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import springframework.sfgpetclinic.services.VetService;
 
 @Controller
 public class VetController {
+
 
     @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
     public String listVets() {
