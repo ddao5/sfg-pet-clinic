0712277b52c42ca87a73ecb9a41fe5e1d5435ba0
Duc Dao <43461796+ddao5@users.noreply.github.com>
2021-05-08 18:29:33 -0400

    Updated visit and pet with JPA mappings. Closes #38

--- pet-clinic-data/src/main/java/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/springframework/sfgpetclinic/model/Pet.java
@@ -2,6 +2,8 @@ package springframework.sfgpetclinic.model;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "pets")
@@ -21,6 +23,10 @@ public class Pet extends BaseEntity {
     @Column(name = "birth_date")
     private LocalDate birthDate;
 
+    @OneToMany(cascade = CascadeType.ALL, mappedBy="pet")
+    private Set<Visit> visits = new HashSet<>();
+
+
     public String getName() {
         return name;
     }
@@ -35,34 +41,47 @@ public class Pet extends BaseEntity {
     public PetType getPetType() {
         return petType;
     }
+
     /**
      * @param petType the petType to set
      */
     public void setPetType(PetType petType) {
         this.petType = petType;
     }
+
     /**
      * @return the owner
      */
     public Owner getOwner() {
         return owner;
     }
+
     /**
      * @param owner the owner to set
      */
     public void setOwner(Owner owner) {
         this.owner = owner;
     }
+
     /**
      * @return the birthDate
      */
     public LocalDate getBirthDate() {
         return birthDate;
     }
+
     /**
      * @param birthDate the birthDate to set
      */
     public void setBirthDate(LocalDate birthDate) {
         this.birthDate = birthDate;
+    }
+
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
     }
 }
