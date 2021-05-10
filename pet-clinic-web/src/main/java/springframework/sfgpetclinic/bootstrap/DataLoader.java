e0661b7cd95fd6affd33b1aa23a649b59636017e
Duc Dao <43461796+ddao5@users.noreply.github.com>
2021-05-10 15:21:11 -0400

    Implemented jpa service for visits. Closes #47

--- pet-clinic-web/src/main/java/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -3,10 +3,7 @@ package springframework.sfgpetclinic.bootstrap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import springframework.sfgpetclinic.model.*;
-import springframework.sfgpetclinic.services.OwnerService;
-import springframework.sfgpetclinic.services.PetTypeService;
-import springframework.sfgpetclinic.services.SpecialtyService;
-import springframework.sfgpetclinic.services.VetService;
+import springframework.sfgpetclinic.services.*;
 
 import java.time.LocalDate;
 
@@ -18,19 +15,23 @@ public class DataLoader implements CommandLineRunner {
     private final VetService vetService;
     private final PetTypeService petTypeService;
     private final SpecialtyService specialtyService;
+    private final VisitService visitService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, SpecialtyService specialtyService) {
+    public DataLoader(OwnerService ownerService, VetService vetService,
+                      PetTypeService petTypeService, SpecialtyService specialtyService,
+                      VisitService visitService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
         this.petTypeService = petTypeService;
         this.specialtyService = specialtyService;
+        this.visitService = visitService;
     }
 
     //id assignment must be refactored
     @Override
     public void run(String... args) throws Exception {
         int count = petTypeService.findAll().size();
-        if(count == 0) {
+        if (count == 0) {
             loadData();
         }
     }
@@ -74,6 +75,12 @@ public class DataLoader implements CommandLineRunner {
         owner2.getPets().add(fionasCat);
 
         ownerService.save(owner2);
+        Visit catVisit = new Visit();
+        catVisit.setPet(fionasCat);
+        catVisit.setDate(LocalDate.now());
+        catVisit.setDescription("Sneezy Kitty");
+
+        visitService.save(catVisit);
 
         System.out.println("Loaded Owners....");
 
